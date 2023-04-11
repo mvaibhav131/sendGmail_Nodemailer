99219be6a82559d4bba0f89b09f5907ef06374ba
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-04-11 16:10:14 +0530

    Feat :sparkles: added comment notes

--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,7 @@ const { email, gmail } = require('../controller/appController');
 
 
  
-router.post('/user/email',email);
+router.post('/user/email',email); //for the email you need these path --> localhost:8080/api/user/email (post Request)
 router.post('/user/gmail',gmail);
 
 module.exports=router;
