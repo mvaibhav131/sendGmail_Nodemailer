5e15288a3c9d8ae128aa5a713f670197b1682233
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-04-11 16:12:57 +0530

    Feat :+1: comment added done for routes
    how to send request is comment added

--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,7 @@ const { email, gmail } = require('../controller/appController');
 
  
 router.post('/user/email',email); //for the email you need these path --> localhost:8080/api/user/email (post Request)
-router.post('/user/gmail',gmail);
+router.post('/user/gmail',gmail); //for the gmail you need these path --> localhost:8080/api/user/gmail (post Request) and
+                                  // send the userEmail address in req.body
 
 module.exports=router;
